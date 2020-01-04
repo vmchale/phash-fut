76e7b3dc68a35e5a2fc09d94502ab0e36b486cf9
Vanessa McHale <vamchale@gmail.com>
2020-01-04 15:22:55 -0600

    Add imagehash?

--- harness.py
+++ harness.py
@@ -14,3 +14,14 @@ mod.img_hash_f32(img0)
 """
 
 print('data/frog.jpeg', timeit.timeit(read_image, setup=setup, number=100) * 10, "ms")
+
+setup_imagehash = """
+from PIL import Image
+import imagehash
+"""
+
+hash_bench = """
+imagehash.phash(Image.open('data/frog.jpeg'))
+"""
+
+print('data/frog.jpeg', timeit.timeit(hash_bench, setup=setup_imagehash, number=100) * 10, "ms")
